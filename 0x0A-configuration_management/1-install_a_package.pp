620df73340431ce23e1905af159889cbf39f5ee6
cynthia402 <cynthiagw10@gmail.com>
2024-05-03 07:13:52 -0700

    corrections

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
+#!/usr/bin/pup
+# Using Puppet, install flask from pip3.
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['Werkzeug'],
+}
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
