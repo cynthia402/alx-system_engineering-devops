e4de3e1aa1100e105436cd2fafa3cfc5e7e6f575
cynthia402 <cynthiagw10@gmail.com>
2024-05-08 06:11:19 -0700

    all tasks done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,25 +1,21 @@
-# Script to install nginx using puppet
+#install nginx and config 
 
-package {'nginx':
-  ensure => 'present',
+package {
+    'nginx':
+    ensure => installed,
 }
 
-exec {'install':
-  command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
-  provider => shell,
-
+file {'/var/www/html/index.html':
+    content => 'Hello World!',
 }
 
-exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-  provider => shell,
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://github.com/Abel-alx-github;\n\t}\n",
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
-  provider => shell,
-}
 
-exec {'run':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+service {'nginx':
+    ensure => running,
 }
