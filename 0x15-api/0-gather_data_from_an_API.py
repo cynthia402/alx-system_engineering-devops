81646cc4e5e988eb5b97825876991f322c90f181
cynthia402 <cynthiagw10@gmail.com>
2024-06-03 09:57:38 -0700

    r

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""  Gather data from an API"""
 import requests
 import sys
 
-if __name__ == "__main__":
-        url = "https://jsonplaceholder.typicode.com/"
-            user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-                todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-                    completed = [t.get("title") for t in todos if t.get("completed") is True]
-                        print("Employee {} is done with tasks({}/{}):".format(
-                                    user.get("name"), len(completed), len(todos)))
-                            [print("\t {}".format(c)) for c in completed]
+def get_todo():
+    id = sys.argv[1]
+    url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+    user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    respond = requests.get(url)
+    users_list = requests.get(user_url)
+
+    name = users_list.json().get('name')
+
+    job_done = 0
+    all_job = 0
+    for row in respond.json():
+        if row.get('completed'):
+            job_done += 1
+        all_job += 1
+    user_profile = f'Employee {name} is done with tasks({job_done}/{all_job}):'
+
+    print(user_profile)
+    for row in respond.json():
+        if row.get('completed'):
+            print("\t", row.get('title'))
+
+
+if __name__ == '__main__':
+    get_todo()
